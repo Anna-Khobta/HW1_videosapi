dd5e67776d4bff40a50d19f142872b7263c0d146
Anna-Khobta <ana14i88@gmail.com>
2023-02-03 00:31:57 +0300

    update delete

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from 'express'
 import bodyParser from "body-parser"
 
-// create express app
+// create express app/
 const app = express()
 const port = process.env.PORT || 3000
 
